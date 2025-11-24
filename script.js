9ad88af039511df1f00c2641af339594f87b3ea1
Zacchouille <59251658+Zacchouille@users.noreply.github.com>
2025-11-24 22:14:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 let pokemonData = [];
 
-fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    pokemonData = data;
-    displayPokemon(data);
-  });
-
 function displayPokemon(pokemons) {
   const container = document.getElementById('pokemon-list');
   container.innerHTML = '';
@@ -16,6 +9,13 @@ function displayPokemon(pokemons) {
     container.appendChild(div);
   });
 }
+
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    pokemonData = data;
+    displayPokemon(pokemonData);
+  });
 
 document.getElementById('search').addEventListener('input', (e) => {
   const searchTerm = e.target.value.toLowerCase();
